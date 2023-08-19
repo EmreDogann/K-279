48fc5c09926c7677e8fc35f8975ae064c1c23876
Emre Dogan <48212096+EmreDogann@users.noreply.github.com>
2023-08-19 12:13:35 +0100

    Added light follower script.

---
+++ Assets/Scripts/Lights/LightFollower.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Lights
+{
+    public class LightFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform followTarget;
+        [SerializeField] private float followSpeed;
+
+        private Vector3 velocity;
+
+        private void Update()
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, followTarget.position, ref velocity,
+                1 / followSpeed, Mathf.Infinity);
+        }
+    }
+}
