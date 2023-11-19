7af10153336dd04c13b5bdcacd917ee7d3ea5472
Emre Dogan <48212096+EmreDogann@users.noreply.github.com>
2023-11-19 22:26:10 +0000

    Refactored fuse slot interactable to work with new refactored interactable system.

---
+++ Assets/Scripts/Interaction/Interactables/FuseInteractable.cs
@@ -0,0 +1,40 @@
+﻿using Interactables;
+using UnityEngine;
+
+namespace Interaction.Interactables
+{
+    [RequireComponent(typeof(FuseSlot))]
+    public class FuseSlotInteractable : InteractableBase
+    {
+        [SerializeField] private FuseSlot fuseSlot;
+
+        private void Awake()
+        {
+            fuseSlot = GetComponent<FuseSlot>();
+        }
+
+        private void Reset()
+        {
+            fuseSlot = GetComponent<FuseSlot>();
+        }
+
+        public override void OnStartHover(IInteractor interactor) {}
+
+        public override void OnStartInteract(IInteractor interactor)
+        {
+            base.OnStartInteract(interactor);
+            switch (interactor.ResolveInteraction(fuseSlot))
+            {
+                case ItemUserInteractionType.Default:
+                    fuseSlot.TryItem(null);
+                    break;
+            }
+        }
+
+        public override void OnInteract(IInteractor interactor) {}
+
+        public override void OnEndInteract(IInteractor interactor) {}
+
+        public override void OnEndHover(IInteractor interactor) {}
+    }
+}
